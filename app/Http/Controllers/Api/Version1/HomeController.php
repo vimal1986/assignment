72b60ec2b384b16ce28acdf8fca6155fb1f04de7
vimal1986 <vimal.adodis@gmail.com>
2018-04-06 18:43:49 +0530

    Site API's

---
+++ app/Http/Controllers/Api/Version1/HomeController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Http\Controllers\Api\Version1;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+use App\Models\Product;
+use App\User;
+use App\Models\UserEnquiry;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try{
+            $model = Product::getModel();
+            $prices = Product::getPrice();
+            $products=Product::getProducts();
+            return response()->json(['status'=>true,
+            'status_code'=>200,
+            'data'=>[
+                'model'=>$model,
+                'price'=>$prices,
+                'products'=>$products
+            ]
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['status'=>false,
+            'status_code'=>500,
+            'message'=>"Server Error"
+            ]);
+        }
+       
+        
+    }
+
+    public function productDetail($product_id){  
+        try{
+            $productDetail = Product::getProductDetail($product_id);
+            return response()->json(['status'=>true,
+            'status_code'=>200,
+            'data'=>[
+                'model'=>$model,
+                'price'=>$prices,
+                'productDetail'=>$productDetail
+            ]
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['status'=>false,
+            'status_code'=>500,
+            'message'=>"Server Error"
+            ]);
+        }      
+        
+       
+    }
+
+    
+
+    public function createUser(Request $request){
+
+
+        try{
+            $rules = [
+                'name' => 'required|max:150',
+                'email' =>'required|email|unique:users,email',
+                'password' => 'min:6',
+                'password_confirmation' => 'required_with:password|same:password|min:6',
+                'mobile'  => 'required|numeric',
+                'address'  => 'sometimes|max:200'
+            ];
+    
+            $validator=  Validator::make($request->all(),$rules);
+    
+            if ($validator->fails()) {
+                return response()->json(['status'=>false,
+                'status_code'=>200,
+                'message'=> $validator->errors()->first()
+                ]);
+               
+            }
+            $result = User::updateOrCreate(
+                [
+                'id'   => $request->get('user_id',0),
+                'name' => $request->get('name'),
+                'email' =>$request->get('email'),
+                'password' => bcrypt($request->get('password')),
+                'mobile'  => $request->get('moblie'),
+                'address'  =>$request->get('address')
+                ]
+            );
+            if ($result) {
+                return response()->json(['status'=>true,
+                'status_code'=>200,
+                'message'=> 'User created Successfully'
+                ]);
+                
+            }
+    
+            return response()->json(['status'=>true,
+            'status_code'=>200,
+            'message'=> 'Not created'
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['status'=>false,
+            'status_code'=>500,
+            'message'=>"Server Error"
+            ]);
+        }      
+        
+    }
+
+    public function siteSearch(Request $request){
+
+
+        try{
+            $model = Product::getModel();
+        $prices = Product::getPrice();
+        $products=Product::getProductSearch($request);
+            return response()->json(['status'=>true,
+            'status_code'=>200,
+            'data'=>[
+                'model'=>$model,
+                'price'=>$prices,
+                'productDetail'=>$products
+            ]
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['status'=>false,
+            'status_code'=>500,
+            'message'=>"Server Error"
+            ]);
+        }      
+        
+
+        
+        
+
+    }
+
+
+    public function productEnquiry(Request $request){
+        try{
+            $rules = [
+                'product_id' => 'required|numeric',
+                'email' =>'required|email'
+            ];
+    
+            $validator=  Validator::make($request->all(),$rules);
+    
+            if ($validator->fails()) {
+                return response()->json(['status'=>false,
+                'status_code'=>200,
+                'message'=> $validator->errors()->first()
+                ]);
+               
+            }
+            $result = UserEnquiry::updateOrCreate(
+                [
+                    'product_id' => $request->get('product_id'),
+                    'email' =>$request->get('email')
+                ]
+            );
+    
+            if ($result) {
+                return response()->json(['status'=>true,
+                'status_code'=>200,
+                'message'=> 'Sent Successfully'
+                ]);
+                
+            }
+    
+            return response()->json(['status'=>false,
+            'status_code'=>200,
+            'message'=> 'Not Sent'
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['status'=>false,
+            'status_code'=>500,
+            'message'=>"Server Error"
+            ]);
+        }      
+    } 
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/site', 'Api\Version1\HomeController@index')->name('site-index');
+
+Route::post('/site', 'Api\Version1\HomeController@siteSearch');
+
+Route::post('/product-enquiry', 'Api\Version1\HomeController@productEnquiry');
+
+Route::get('/register', 'Api\Version1\HomeController@register')->name('register');
+Route::post('/register', 'Api\Version1\HomeController@createUser');
+
+Route::get('/site/product-detail/{id}', 'Api\Version1\HomeController@productDetail')->name('site-product-detail');
